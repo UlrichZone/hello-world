17b098f4a99067e10d1aae7f46d0b67426459235
yz.liu <korns@foxmail.comgit>
2022-09-27 21:21:15 +0800

    new

---
+++ test.py
@@ -0,0 +1,21 @@
+print('Hello World')
+
+'''
+
+Your public key has been saved in /Users/likorn/.ssh/id_rsa.pub.
+The key fingerprint is:
+SHA256:SrM0XLzkLlVZcGoe8txnO/OcPfR/3pPYrt+o0mbOpJ4 korns@foxmail.com
+The key's randomart image is:
++---[RSA 3072]----+
+|          ..o    |
+|       .   =     |
+|        = *      |
+|     . + O o     |
+|      * S + . o  |
+|     o B     o o |
+|      + .  .. B o|
+|       .  .=+. @B|
+|         .E=+o==&|
++----[SHA256]-----+
+
+'''
